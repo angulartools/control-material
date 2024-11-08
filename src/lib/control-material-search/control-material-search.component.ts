889cbe3e7512cae45c3a8e301f315bc2fdae648d
danieleespindola-brenntag <daniele.espindola@brenntag-external.com>
2024-11-08 10:41:50 -0300

    Ajustes nos componentes

---
+++ src/lib/control-material-search/control-material-search.component.ts
@@ -0,0 +1,118 @@
+import { NG_VALUE_ACCESSOR, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AfterContentInit, Component, forwardRef, Input } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgClass, AsyncPipe, DatePipe } from '@angular/common';
+import { MatOption } from '@angular/material/core';
+import { NgbPopover } from '@ng-bootstrap/ng-bootstrap';
+import { MatIconButton } from '@angular/material/button';
+import { MatTooltip } from '@angular/material/tooltip';
+import { MatIcon } from '@angular/material/icon';
+import { MatAutocompleteTrigger, MatAutocomplete } from '@angular/material/autocomplete';
+import { MatInput } from '@angular/material/input';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { MatFormField, MatLabel, MatSuffix, MatPrefix, MatError } from '@angular/material/form-field';
+import { ControlMaterialAutocompleteComponent, searchControlValidator } from '../control-material-autocomplete/control-material-autocomplete.component';
+import { catchError, debounceTime, distinctUntilChanged, filter, Observable, of, switchMap, tap } from 'rxjs';
+
+@Component({
+    selector: 'lib-control-material-search',
+    templateUrl: './control-material-search.component.html',
+    styleUrls: ['../control-material.component.scss', '../control-material-autocomplete/control-material-autocomplete.component.scss'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => ControlMaterialSearchComponent), // replace name as appropriate
+            multi: true
+        }
+    ],
+    standalone: true,
+    imports: [MatFormField, MatLabel, MatProgressSpinner, MatSuffix, MatInput, FormsModule, MatAutocompleteTrigger, ReactiveFormsModule, MatPrefix, MatIcon, MatTooltip, MatIconButton, NgbPopover, MatError, MatAutocomplete, MatOption, NgClass, AsyncPipe, DatePipe, TranslateModule]
+})
+export class ControlMaterialSearchComponent extends ControlMaterialAutocompleteComponent implements AfterContentInit {
+
+  @Input() service: any;
+  @Input() metodo = 'pesquisar';
+  @Input() filtrarNulo = false;
+
+  @Input('parametroPesquisa')
+  set setParametroPesquisa(value) {
+    this.parametroPesquisa = value;
+  }
+  parametroPesquisa = null;
+  filteredSearch: Observable<any>;
+  searchFailed = false;
+
+  get value() {
+    return this.control.value;
+  }
+
+  set value(val) {
+    this.control.setValue(val);
+
+    this.onChange(val);
+    this.onTouched();
+  }
+
+  override ngAfterContentInit() {
+    super.ngAfterContentInit();
+
+    if (this.control !== undefined){
+      if (this.required) {
+        this.control.setValidators([Validators.required, searchControlValidator]);
+      }
+
+      if (this.parametroPesquisa !== undefined) {
+        this.filteredSearch = this.control.valueChanges
+        .pipe(
+          debounceTime(300),
+          distinctUntilChanged(),
+          tap(() =>
+          {
+            this.loading = true;
+            this.loadingData =  true;
+          }),
+          switchMap((value) =>
+            this.service[this.metodo](value === '' || value === null || value[this.bindLabel] === undefined ? value : value[this.bindLabel], this.parametroPesquisa, this.bindId).pipe(
+              tap(() => {
+                this.searchFailed = false;
+                this.loading = false;
+                this.loadingData =  false;
+              }),
+              catchError(() => {
+                this.searchFailed = true;
+                this.loading = false;
+                this.loadingData =  false;
+                return of([]);
+              }))
+          ),
+        );
+      } else {
+        this.filteredSearch = this.control.valueChanges
+        .pipe(
+          debounceTime(300),
+          distinctUntilChanged(),
+          filter(value => value !== '' && value !== null && !this.filtrarNulo),
+          tap(() =>
+          {
+            this.loading = true;
+            this.loadingData =  true;
+          }),
+          switchMap(value =>
+            this.service[this.metodo](value[this.bindLabel] === undefined ? value : value[this.bindLabel], this.bindId).pipe(
+              tap(() => {
+                this.searchFailed = false;
+                this.loading = false;
+                this.loadingData =  false;
+              }),
+              catchError(() => {
+                this.searchFailed = true;
+                this.loading = false;
+                this.loadingData =  false;
+                return of([]);
+              }))
+          ),
+        );
+      }
+    }
+  }
+}
--- src/public-api.ts
+++ src/public-api.ts
@@ -12,6 +12,7 @@ export * from './lib/control-material-number/control-material-number.component';
 export * from './lib/control-material-password/control-material-password.component';
 export * from './lib/control-material-phone/control-material-phone.component';
 export * from './lib/control-material-radio/control-material-radio.component';
+export * from './lib/control-material-search/control-material-search.component';
 export * from './lib/control-material-select/control-material-select.component';
 export * from './lib/control-material-textarea/control-material-textarea.component';
 export * from './lib/control-material-time/control-material-time.component';
