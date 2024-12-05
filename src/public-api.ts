d011e8208aa7aa4a3afcecc662977205d79690ff
danieleespindola-brenntag <daniele.espindola@brenntag-external.com>
2024-12-05 16:53:53 -0300

    Adicionado ao export

--- src/public-api.ts
+++ src/public-api.ts
@@ -18,6 +18,7 @@ export * from './lib/control-material-masked/control-material-masked.component';
 export * from './lib/control-material-minute-second/control-material-minute-second.component';
 export * from './lib/control-material-number/control-material-number.component';
 export * from './lib/control-material-password/control-material-password.component';
+export * from './lib/control-material-password-strength/control-material-password-strength.component';
 export * from './lib/control-material-phone/control-material-phone.component';
 export * from './lib/control-material-radio/control-material-radio.component';
 export * from './lib/control-material-search/control-material-search.component';
